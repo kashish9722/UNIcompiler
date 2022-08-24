77c15b1a6b49bb47581924a4d24b851f1e85de22
kashish <73249916+kashish9722@users.noreply.github.com>
2022-08-24 20:05:10 +0530

    Add files via upload

---
+++ mp3player.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+pip install playsound
+
+
+# In[4]:
+
+
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('D:\Downloads\sample.mp3')
+pygame.mixer.music.play()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
